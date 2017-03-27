42b8552b64b800b41ff9aaa1de48ff01c0d9fa92
fanhehe <13889441200@163.com>
2017-03-27 13:25:12 +0800

    add register

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -3,8 +3,21 @@ export default (ctx) => {
   ctx.body = ctx.request.body
 }
 
-export function login (ctx, body) {
-  ctx.body = ctx.request.body
+export async function Login (ctx) {
+    const { username, password } = ctx.request.body;
+    if (!(username && password)) return ctx.body = { code: 400, msg: '请输入账号密码' };
+    const result = await TUser.find({ where: { username, password }, attributes: ['id', 'username']});
+    if (!(result && result.id)) return ctx.body = { code: 400, msg: '账号密码错误' };
+    ctx.session = { id: result.id,  username };
+    return ctx.body = { code: 200, msg: '登陆成功', data: result };
 }
-
-console.log(TUser.find)
+export async function Register (ctx) {
+    const { username, password } = ctx.request.body;
+    if (!(username && password)) return ctx.body = { code: 400, msg: '请输入账号密码' };
+    const result = await TUser.find({ where: { username, password }, attributes: ['id', 'username']});
+    if (result && result.id) return ctx.body = { code: 400, msg: '账号已存在' };
+    const status = await TUser.create({ username, password });
+    delete status.dataValues.password;
+    ctx.session = { id: status.id,  username };
+    return ctx.body = { code: 200, msg: '注册成功', data: status };
+}
